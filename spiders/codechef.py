1f323a536e1592fbcfa00591de8fb234f9aa6b5c
Blaze <maanikhurana1007@gmail.com>
2024-10-26 23:23:47 +0530

    Working table data is getting submotted as json changes in codechef.py

--- spiders/codechef.py
+++ spiders/codechef.py
@@ -19,20 +19,17 @@ class CodechefSpider(scrapy.Spider):
     def parse(self, response):
         data = []
         for row in response.css("table.dataTable > tbody > tr"):
-            time = row.css("td:nth-child(1) .tooltiptext::text").get()
+            
             problem = row.css("td:nth-child(2) a::text").get()
-            problem_title = row.css("td:nth-child(2) a::attr(title)").get()
+            
             result = row.css("td:nth-child(3) span::attr(title)").get()
-            language = row.css("td:nth-child(4)::text").get()
-            solution_link = row.css("td:nth-child(5) a::attr(href)").get()
+           
 
             data.append({
-                "time": time,
                 "problem": problem,
-                "problem_title": problem_title,
                 "result": result,
-                "language": language,
-                "solution_link": solution_link,
+                
+                
             })
 
         yield {"data": data}
