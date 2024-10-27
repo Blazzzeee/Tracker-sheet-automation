6909d1f1fd5350c73c39d787b9c9c72670ccfa1e
Blaze <maanikhurana1007@gmail.com>
2024-10-27 16:10:47 +0530

    removed refactoring

--- spiders/codechef.py
+++ spiders/codechef.py
@@ -7,18 +7,7 @@ class CodechefSpider(scrapy.Spider):
     start_urls = ["https://codechef.com/"]
 
     def start_requests(self):
-        """
-        Instead of scraping the page source as-is, we want to load the page using
-        a headless browser and wait for the page to render the content using
-        JavaScript. We then wait for a specific element to appear on the page
-        (in this case, a table with the class "dataTable"). Once the element
-        appears, we yield the response to the `parse` method, which will then
-        extract the desired information from the page.
-
-        This approach is useful when the page uses a lot of JavaScript to load
-        its content, and the page source does not contain the information we
-        need.
-        """
+        
         yield scrapy.Request(
             url="https://www.codechef.com/users/vashuvats1",
             callback=self.parse,
@@ -31,21 +20,7 @@ class CodechefSpider(scrapy.Spider):
         )
 
     def parse(self, response):
-        """
-        Extract information from a page about a Codechef user's submissions.
-
-        The page is expected to contain a table with the class "dataTable" which
-        contains the user's submissions. The table should have the following columns:
-
-        - Problem name
-        - Result
-
-        The function yields a dictionary containing the user's submissions, with
-        the problem name as the key and the result as the value.
-
-        :param response: The response object containing the page source
-        :yield: A dictionary containing the user's submissions
-        """
+        
         data = []
         for row in response.css("table.dataTable > tbody > tr"):
             
